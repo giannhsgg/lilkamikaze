0e984071daed8041be9688576d3a724284195d62
OGG <48725382+giannhsgg@users.noreply.github.com>
2019-04-06 00:47:48 +0300

    Update kamikaze.py

--- kamikaze.py
+++ kamikaze.py
@@ -1,24 +1,40 @@
-﻿import discord
+import discord
 from discord.ext import commands
 import asyncio
+import re
 
-client = discord.Client()
-bot = commands.Bot(command_prefix=".")
 
-#Bot Ready event
+bot = commands.Bot(command_prefix='!!')
+
+
+#Bot Ready
 @bot.event
 async def on_ready():
-    print('Logged in as')
-    print(bot.user.name)
-    print(bot.user.id)
-    print('this mothafacka is ready boss!')
-    print('------')
+    print("♥ Bot",bot.user.name, "is starting up.")
+    print("♥ Id :",bot.user.id)
+    print("♥ Ready boss!(っ◔◡◔)っ")
 
+#"commands"
 @bot.event
 async def on_message(message):
-    if message.content == "cookie":
-        await client.delete_message(message)
-        await client.send_message(message.author, ":cookie:")
+    if message.content.lower() == "oggs":
+        await bot.process_commands(message)
+        await bot.send_message(message.channel, " @OGG Bot#3905 is here for you homie")
+    if message.content.lower() == "cookie":
+        await bot.process_commands(message)
+        await bot.send_message(message.channel, ":cookie:")
+    if message.content.lower() == "poutsa":
+        await bot.process_commands(message)
+        await bot.send_message(message.channel, ":eggplant:")
+    if message.content.lower() == "rebas":
+        await bot.process_commands(message)
+        await bot.send_message(message.channel, ":middle_finger:")
 
+@bot.command
+async def testembed(ctx):
+    embed = discord.Embed(title="xes", description="Gamaei", colour=discord.Color.red(), url="www.google.com")
+    await ctx.send(embed=embed)
+
+ 
 bot.run('NTYxODU3OTgwNTEzOTEwODE0.XKCVIw.OKfecLRHcG-QP0DdQiAeaOsdPy8')
 
